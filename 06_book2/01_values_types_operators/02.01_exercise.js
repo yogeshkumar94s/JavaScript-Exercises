de06b85c1129c80a9425a053026949d37a6e73cd
Yogesh Kumar <yksainimbd@gmail.com>
2023-11-30 19:22:39 +0530

    updates

---
+++ 06_book2/01_values_types_operators/02.01_exercise.js
@@ -0,0 +1,11 @@
+// Create a new string variable named greeting and assign it the value "Good" concatenated with singleQuotesString and then "Morning!".
+
+let greet1 = "good";
+let greet2 = "morning";
+console.log(`concatenated string is: ${greet1} ${greet2}`);
+
+// Create a new string variable named newString and assign it any text you like.
+let newString = "this is my new string!";
+// Calculate and store the length of newString in a variable called newStringLength.
+let newStringLength = newString.length;
+console.log(newStringLength);
--- 06_book2/01_values_types_operators/07_short_circuiting.js
+++ 06_book2/01_values_types_operators/07_short_circuiting.js
@@ -30,3 +30,39 @@ let isValid =
 // In this case, if userInput is undefined or null, the subsequent checks (userInput.trim() !== "") will not be executed, improving performance.
 
 // Understanding short-circuiting can lead to more efficient and concise code, especially in scenarios where further evaluation is unnecessary based on the initial conditions.
+
+// Real case example of short-circuiting with logical AND*-*-*-*-**-*-
+function greetUser(username) {
+  // Using short-circuiting to provide a default value if username is undefined or null
+  const greeting = "Hello, " + (username && username.toLowerCase()) + "!";
+  console.log(greeting);
+}
+
+// Example usages:
+greetUser("John"); // Output: Hello, john!
+greetUser(); // Output: Hello, undefined!
+
+// Real case example of short-circuiting with logical OR*-*-*-*-**-***-*
+function getUserDetails(user) {
+  // Using short-circuiting to provide a default name if user name is not provided
+  const displayName = user.name || "Guest";
+
+  // Using short-circuiting to provide a default age if age is not provided or is not a number
+  const userAge = user.age || 25;
+
+  console.log("Display Name:", displayName);
+  console.log("User Age:", userAge);
+}
+
+// Example usages:
+getUserDetails({ name: "John", age: 30 });
+// Output: Display Name: John
+//         User Age: 30
+
+getUserDetails({ age: 28 });
+// Output: Display Name: Guest
+//         User Age: 28
+
+getUserDetails({});
+// Output: Display Name: Guest
+//         User Age: 25
