b047235ed4ea50b3522fe58adee365b7be4974ab
Yogesh Kumar <yksainimbd@gmail.com>
2024-11-17 10:12:13 +0530

    delete some files

--- 01_basics/06_problem.js
+++
@@ -1,9 +0,0 @@
-// Q: Write a JavaScript program to determine whether a given year is a leap year
-
-const checkLeapYear = (year) => {
-  if (year % 4 === 0) return `The year :  ${year} is a leap year`;
-
-  return `The year : ${year} is not a leap year`;
-};
-
-console.log(checkLeapYear(2025));
--- 01_basics/07_problem.js
+++
@@ -1,1 +0,0 @@
-// Q:  Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050.
